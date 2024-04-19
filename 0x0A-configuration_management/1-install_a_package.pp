4de0fabb4df9ced86ac356b2fa827a7d37e8bb71
ben obame <obameben@yahoo.fr>
2024-04-19 13:03:29 +0200

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# First ensure pip3 is installed
-exec { 'install Flask':
-  command     => 'pip3 install Flask==2.1.0',
-  unless      => 'pip3 freeze | grep Flask==2.1.0',
-  path        => ['/usr/bin', '/bin'],
-  provider    => 'shell',
+# install puppet-lint -v 2.5.0
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
