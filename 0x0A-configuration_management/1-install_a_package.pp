45975aba5e15b64fc63e82c9f4053096f773b3b2
ben obame <obameben@yahoo.fr>
2024-04-19 12:21:29 +0200

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Filename: 1-install_a_package.pp
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',                # Specifies the version of Flask to install
-  provider => 'gem',                 # Specifies that pip3 should be used to install the package
+  provider => 'pip3',                 # Specifies that pip3 should be used to install the package
 }
