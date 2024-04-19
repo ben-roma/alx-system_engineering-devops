890d4620804f180baeac370300c799c006ab20ac
ben obame <obameben@yahoo.fr>
2024-04-19 12:46:42 +0200

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
 # First ensure pip3 is installed
-exec { 'install-pip3':
-  command => '/usr/bin/apt-get install -y python3-pip',
-  unless  => 'dpkg -l | grep -qw python3-pip',
-}
-
-# Install Flask via pip3
-exec { 'install-flask':
-  command => '/usr/local/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/local/bin/pip3 freeze | grep -q "Flask==2.1.0"',
-  require => Exec['install-pip3'], # Ensure pip3 is installed before running this
+exec { 'install Flask':
+  command     => 'pip3 install Flask==2.1.0',
+  unless      => 'pip3 freeze | grep Flask==2.1.0',
+  path        => ['/usr/bin', '/bin'],
+  provider    => 'shell',
 }
