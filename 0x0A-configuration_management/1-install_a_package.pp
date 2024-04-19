d363a40726fa038ce1895ade92f85faf74f48fff
ben obame <obameben@yahoo.fr>
2024-04-19 13:12:01 +0200

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # install Flask -v 2.1.0
 
-exec { 'Flask':
-  command => '/usr/bin/apt-get -y install Flask -v 2.1.0',
+exec { 'install specific version of flask using pip3':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
 }
