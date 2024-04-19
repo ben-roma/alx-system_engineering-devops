f54bb183f6bccec3a5cd2453972d323e5af4a70f
ben obame <obameben@yahoo.fr>
2024-04-19 13:23:15 +0200

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,8 @@
 # Puppet manifest to install Flask version 2.1.0 using pip3
 class flask_install {
-  package { 'python3-pip':
-	ensure => installed,
-  }
-
-  exec { 'install Flask':
-	command     => 'pip3 install Flask==2.1.0',
-	unless      => 'pip3 freeze | grep Flask==2.1.0',
-	path        => ['/usr/bin', '/bin'],
-	require     => Package['python3-pip'],  # Ensure pip is installed first
+  package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
   }
 }
 
