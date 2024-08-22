7622b4fce41170ac272a4068d8aae5096494a089
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2024-08-22 18:27:22 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,16 @@
 # Increase the ULIMIT in the /etc/default/nginx file
 
 exec { 'increase-nginx-ulimit':
-  command => 'sed -i "s/^ULIMIT=.*/ULIMIT=4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
-  onlyif  => 'grep -q "^ULIMIT=15" /etc/default/nginx',
-  notify  => Exec['nginx-restart'],
+    command => 'sed -i "s/^ULIMIT=.*/ULIMIT=4096/" /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/',
+    onlyif  => 'grep -q "^ULIMIT=15" /etc/default/nginx',
+    notify  => Exec['nginx-restart'],
 }
 
 # Restart Nginx
 
 exec { 'nginx-restart':
-  command     => '/etc/init.d/nginx restart',
-  path        => '/sbin/:/usr/sbin/:/bin/:/usr/bin/',
-  refreshonly => true,
+    command     => '/etc/init.d/nginx restart',
+    path        => '/sbin/:/usr/sbin/:/bin/:/usr/bin/',
+    refreshonly => true,
 }
